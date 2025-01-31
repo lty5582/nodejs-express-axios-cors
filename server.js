f5370302da5796aacb33c5bb20f05a16803e83c5
dwqcwda <lty5582@naver.com>
2025-01-31 12:10:14 +0900

    node day 2

--- server.js
+++ server.js
@@ -1,61 +1,38 @@
 // server.js
 
-const http = require('http');
+const express = require('express');
+const CORS = require('cors');
+const app = express();
 
 // CORS 설정을 위한 헤더
-const headers = {
-  'Access-Control-Allow-Origin': "http://127.0.0.1:9000",
-  'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, PUT, DELETE',
-  'Access-Control-Allow-Headers': 'Content-Type',
-};
+app.use(CORS({
+  origin: 'http://localhost:5502',
+  methods : ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE'], 
+}));
+
+app.use(express.text());
 
 let data = { message: '여러분 화이팅!' };
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'OPTIONS') {
-    res.writeHead(204, headers);
-    res.end();
-    return;
-  }
-
-  if (req.method === 'GET') {
-    res.writeHead(200, { 'Content-Type': 'application/json', ...headers });
-    res.end(JSON.stringify(data));
-  }
-
-  if (req.method === 'POST') {
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on('end', () => {
-      data.message = body;
-      res.writeHead(200, headers);
-      res.end(`받은 POST 데이터: ${body}`);
-    });
-  }
-
-  if (req.method === 'PUT') {
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on('end', () => {
-      data.message = body;
-      res.writeHead(200, headers);
-      res.end(`업데이트된 데이터: ${body}`);
-    });
-  }
-
-  if (req.method === 'DELETE') {
-    data = {};
-    res.writeHead(200, headers);
-    res.end('데이터가 삭제되었습니다.');
-  }
+app.get('/message', (req, res) => {
+  res.json(data);
 });
 
-server.listen(3000, () => {
-  console.log('서버가 http://localhost:3000/ 에서 실행 중입니다.');
+app.post('/message', (req, res) => {
+  data.message = req.body;
+  res.send(`받은 POST 데이터: ${req.body}`);
+});
+
+app.put('/message', (req, res) => {
+  data.message = req.body;
+  res.send(`업데이트된 데이터: ${req.body}`);
+});
+
+app.delete('/message', (req, res) => {
+  data = {};
+  res.send('데이터가 삭제되었습니다.');
+});
+
+app.listen(3000, () => {
+  console.log('Express 서버가 http://localhost:3000/ 에서 실행 중입니다.');
 });
